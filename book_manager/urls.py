edf58f5e82a4afbdb419f816822842ce311810a9
LazarA3 <alexandru.lazar2@flutteruki.com>
2026-07-23 20:54:40 +0300

    updated login views

--- book_manager/urls.py
+++ book_manager/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('books.urls')),
+    path('', include('accounts.urls')),
+    path( "accounts/", include('accounts.urls')),
 ]
 
 if settings.DEBUG:
